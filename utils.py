3d5ea0bcca52aae3bda136392f44a81595b823a2
1125-a <3045151408@qq.com>
2024-10-21 17:17:34 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,20 @@
+# utils.py
+
+import numpy as np
+from collections import deque
+import random
+
+class ReplayBuffer:
+    def __init__(self, max_size=100000):
+        self.buffer = deque(maxlen=max_size)
+
+    def add(self, state, action, reward, next_state, done):
+        self.buffer.append((state, action, reward, next_state, done))
+
+    def sample(self, batch_size):
+        batch = random.sample(self.buffer, batch_size)
+        states, actions, rewards, next_states, dones = map(np.array, zip(*batch))
+        return states, actions, rewards, next_states, dones
+
+    def size(self):
+        return len(self.buffer)
